75c4a64257736de65761fca19722c669d596d6ca
Anh <anhtnt@elightup.com>
2017-10-24 12:10:36 +0700

    Combine JS, use instant click and add SEO

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source "https://rubygems.org"
 
+gem 'sprockets', '~> 3.0'
 gem 'jekyll'
+gem 'jekyll-seo-tag'
+gem "jekyll-assets", {
+  group: :jekyll_plugins
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,43 @@
-function loadGoogleFonts() {
-	window.WebFontConfig = {
-		google: { families: [ 'Roboto+Slab:700' ] }
+// = require tocbot.min.js
+// = require instantclick.min.js
+
+function toggleMobileMenu() {
+	var container, button, menu;
+
+	container = document.getElementById( 'site-navigation' );
+	if ( ! container ) {
+		return;
+	}
+
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button ) {
+		return;
+	}
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
+
+	menu.setAttribute( 'aria-expanded', 'false' );
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+		menu.className += ' nav-menu';
+	}
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			container.className += ' toggled';
+			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
 	};
-	var wf = document.createElement('script'), s = document.scripts[0];
-	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-	wf.async = true;
-	s.parentNode.insertBefore(wf, s);
 }
 
 function highlighCurrentDocs() {
@@ -90,7 +122,23 @@ function generateTOC() {
 	} );
 }
 
-loadGoogleFonts();
-toggleDocsMenu();
-filterDocs();
-generateTOC()
+function init() {
+	toggleMobileMenu();
+	toggleDocsMenu();
+	filterDocs();
+	generateTOC();
+}
+
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-57415220-10');
+
+// Google Fonts.
+window.WebFontConfig = {
+	google: { families: [ 'Roboto+Slab:700' ] }
+};
+
+InstantClick.on('change', init);
+InstantClick.init();
